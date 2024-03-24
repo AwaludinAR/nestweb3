da41c1a39964a05eef4e8a01a857f69c94396fb3
Awaludin AR <me@awaludin.dev>
2024-03-25 05:23:35 +0700

    wip: 1

--- release.config.cjs
+++ release.config.cjs
@@ -2,13 +2,41 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  repositoryUrl: 'https://github.com/awaludinar/nestweb3.git',
-  branches: ['action-test'],
+  repositoryUrl: 'https://github.com/AwaludinAR/nestweb3.git',
+  branches: [
+    { nam: 'action-test' },
+    { name: 'next', channel: 'next', prerelease: true },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: './CHANGELOG.md',
+        changelogTitle: '# Changelog',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          'dist/**/*.{js,d.ts}',
+          'CHANGELOG.md',
+          'LICENSE',
+          'README.md',
+          'package.json',
+          'package-lock.json',
+        ],
+        draftRelease: true,
+      },
+    ],
     '@semantic-release/npm',
-    '@semantic-release/github',
-    '@semantic-release/git',
   ],
 };
