659f18d4c5fd409d9093f568bf10d2c1f99add20
Awaludin AR <me@awaludin.dev>
2024-03-27 19:58:04 +0700

    wip: clean code

--- lib/web3.constants.ts
+++ lib/web3.constants.ts
@@ -1,5 +1,4 @@
 export const DEFAULT_WEB3_CONNECTION = 'Web3Connection';
-export const WEB3_MODULE_OPTIONS = 'Web3ModuleOptions';
 export const WEB3_CONNECTION_NAME = 'Web3ConnectionName';
 export enum Web3Provider {
   HTTP = 'HttpProvider',
--- lib/web3.providers.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleRef } from '@nestjs/core';
-import { Web3, HttpProvider, WebSocketProvider } from 'web3';
-
-export function createWeb3Providers(connectionName: string) {}
